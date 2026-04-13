85f2df4a6bb4012de835202cce897a2bcde89a6d
Yusuf-Saiyad <yusufsaiyad2002@gmail.com>
2026-04-13 19:21:17 +0530

    Conditional Rendering

--- ReactSetup/src/Components/Card.jsx
+++ ReactSetup/src/Components/Card.jsx
@@ -6,17 +6,20 @@ function Card(){
     {
         image: 'https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?q=80&w=1413&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name:"Head Phones",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque."
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque.",
+        instock: true
     },
     {
         image: 'https://images.unsplash.com/photo-1543512214-318c7553f230?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name:"Boat Speaker",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque."
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque.",
+        instock:false
     },
     {
         image: 'https://plus.unsplash.com/premium_photo-1664110691129-ca0f4fbe8533?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name:"Personal Diary",
-        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque."
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque.",
+        instock:false
     },
     
 
@@ -36,7 +39,10 @@ function Card(){
                 <div className="w-full px-3 py-4">
                     <h2 className="font-semibold">{elem.name }</h2>
                     <p className="text-xs mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore rem fugit autem voluptatem? Repellat perferendis molestiae nostrum sed, modi cum cupiditate. In porro, accusamus sint soluta tenetur quisquam a atque. </p>
-                </div>
+                        <button className={`px-4 py-1 ${elem.instock ? "bg-blue-600" : "bg-red-600"}  text-xs rounded text-zinc-100 mt-3`}>
+                            {elem.instock ? 'In Stock' : "Out Of Stock" }
+                            </button>
+                    </div>
             </div>
             ))}
         </div> 
