1931f9217e0b861ae6f012571d96d56baa453679
Yusuf-Saiyad <yusufsaiyad2002@gmail.com>
2026-04-14 18:48:53 +0530

    Learn how add element in array of object in state

--- ReactSetup/src/App.jsx
+++ ReactSetup/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 
 function App() {
- const [val,setVal] = useState([1,2,3,4,5,6])
+ const [val,setVal] = useState([
+    {name:"Yusuf",age:23},
+    {name:"Wasib",age:24},
+    {name:"Aayush",age:22}
+])
   return (
     <div className='p-5'>
-        {val.map(item=><h1>{item}</h1>)}
+            {val.map((item)=>(
+                <div>
+                    <h1>{item.name}</h1>
+                    <h2>{item.age}</h2>
+                </div>
+            ))}
         <button  onClick={()=>setVal(()=>
-        val.filter(item=>item%2!==0))}  className='mt-3 px-3 py-1 rounded-full text-xs text-white bg-blue-600'>Click</button>
+        val.map(item=>item.name==="Wasib" ? ({name:"Wasib",age:25}):item))}  className='mt-3 px-3 py-1 rounded-full text-xs text-white bg-blue-600'>Click</button>
     </div>
   )
 }
