dc92b86e394a9dafe1238f80bdc3cb041482cbe0
Yusuf-Saiyad <yusufsaiyad2002@gmail.com>
2026-04-14 16:54:20 +0530

    Managing State and Understanding state hook

--- ReactSetup/src/App.jsx
+++ ReactSetup/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Card from './Components/Card';
-
-
+import React, { useState } from 'react'
 
 function App(){
 
-    
+    const [val,setVal]= useState({name:"Yusuf", isBanned: false});
+
   return(
-      <div>
-          <Card></Card>
-      </div>
+    <div className='p-4'>
+        <h1>name : {val.name}</h1>
+        <h2>Banned: {val.isBanned.toString()}</h2>
+        <button onClick={()=>setVal({...val,  isBanned : !val.isBanned})} className='px-3 py-1 bg-blue-500 rounded-full text-white'>Change</button>
+
+    </div>
   )
 }
 
