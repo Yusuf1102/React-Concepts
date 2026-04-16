1fa749e45c957b4c75412bd50f5b67635145439f
Yusuf-Saiyad <yusufsaiyad2002@gmail.com>
2026-04-16 08:36:05 +0530

    Understand Props and its use

--- ReactSetup/src/App.jsx
+++ ReactSetup/src/App.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Components/Card'
 
 function App() {
- 
+    const data = [
+
+          {   name: "Yusuf",
+              profession: "Full Stack Developer",
+              image: "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+               friends: false
+          },
+          {   name: "Wasib",
+              profession: "Doctor",
+              image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+               friends: false
+          },
+          {   name: "Sahil",
+              profession: "Lawyer",
+              image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              friends: false
+          },
+          {   name: "Neet",
+              profession: "Software Engineer",
+              image: "https://images.unsplash.com/photo-1600080695930-6af670ad44fb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+               friends: false
+          }
+    ]
+  const [realdata,setRealData]  = useState(data) 
+  const handleFriendsButton = (cardIndex)=>{
+            setRealData((previous)=>{
+                    return previous.map((item,index)=>{
+                            if(index==cardIndex){
+                                return {...item, friends: !item.friends}
+                            }
+                            return item;
+                    })
+            })
+  }
+  
   return (
-    <div className='p-5'>
-       <Card text = "Know More" color = "bg-blue-600"/>
-       <Card text = "Download" color = "bg-red-600"></Card>
+    <>
+    <div className="w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center">
+        {realdata.map((item,index)=>(
+            <Card key= {index} index={index} handleClick = {handleFriendsButton} values = {item} />  
+        ))}
     </div>
+    </>
   )
 }
 
--- ReactSetup/src/Components/Card.jsx
+++ ReactSetup/src/Components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Card({text,color}) {
+function Card({values,handleClick,index}) {
+  const {image,name,profession,friends} = values;
   return (
-   <button className={`px-3 py-1 ${color} text-zinc-100 text-xs rounded m-2`}>
-     {text}</button>
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+        <div className='w-full h-40 bg-sky-200 '>
+            <img className='w-full h-full object-cover object-[center_  top] ' src={image} alt="" />
+           </div>
+        <div className='w-full p-3'>
+            <h3 className='text-xl font-semibold'>{name}</h3>
+            <h5 className='text-xs'>{profession}</h5>
+            <button onClick={()=>handleClick(index)} className='px-3 py-1 mt-4 text-xs text-white bg-blue-500 
+               font-semibold rounded-md'>{friends ? "Friends" : "Add Friends "}</button>
+        </div>
+    </div>
   )
 }
 
