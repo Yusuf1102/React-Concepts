c04fb29218e8feaadccd875e8969fea9c030179d
Yusuf-Saiyad <yusufsaiyad2002@gmail.com>
2026-04-16 15:45:09 +0530

    Completed a Exercise building components and using state and props for data rendering

--- ReactSetup/src/Components/Card.jsx
+++ ReactSetup/src/Components/Card.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 
-function Card({values,handleClick,index}) {
-  const {image,name,profession,friends} = values;
+function Card({data,handleClick,index} ) {
+  const {name,image,artist,added} = data;
   return (
-    <div className='w-52 bg-white rounded-md overflow-hidden'>
-        <div className='w-full h-40 bg-sky-200 '>
-            <img className='w-full h-full object-cover object-[center_  top] ' src={image} alt="" />
-           </div>
-        <div className='w-full p-3'>
-            <h3 className='text-xl font-semibold'>{name}</h3>
-            <h5 className='text-xs'>{profession}</h5>
-            <button onClick={()=>handleClick(index)} className='px-3 py-1 mt-4 text-xs text-white bg-blue-500 
-               font-semibold rounded-md'>{friends ? "Friends" : "Add Friends "}</button>
+    <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-10 relative mt-20'>
+        <div className='w-20 h-20 bg-orange-600 rounded-md overflow-hidden' >
+            <img className='w-full h-full object-cover' src={image} alt="" />
         </div>
+        <div>
+          <h3 className='text-xl leading-none font-semi-bold'>{name}</h3>
+          <h6 className='text-sm'>{artist} </h6>
+        </div>
+        <button onClick={()=>handleClick(index)} className={`px-4 py-3 whitespace-nowrap ${added ? "bg-teal-600" : "bg-orange-600"} absolute bottom-0 left-1/2 -translate-x-[50%] translate-y-[50%] text-white text-xs rounded-full `}>{added === false ? "Add to Favourties" : "Added"}</button>
     </div>
   )
 }
